58df35b8ed9aaf6546c87d1d6823d2be8e05f9ff
Saketh Ayyagari <sakethsarma07@gmail.com>
2024-09-07 19:52:41 -0400

    prevented integrator windup for line following

--- labs/lab4/LineFollower.py
+++ labs/lab4/LineFollower.py
@@ -39,28 +39,32 @@ rc = None
 global speed, angle
 global Kp, Ki, Kd
 Kp = 0.003125
-Ki = 0.003125
-Kd = 0.003#0925
+Ki = 0.001
+Kd = 0.0003
+
+error_sum = 0
+time = []
+error = []
 
 # Previous error for derivative
 prevError = None
 # cumulative error for integral
 sum = 0
 # The smallest contour we will recognize as a valid contour
-MIN_CONTOUR_AREA = 45
+MIN_CONTOUR_AREA = 55
 
 
 # TODO Part 1: Determine the HSV color threshold pairs for BLUE, GREEN, and RED
 
 BLUE = ((80, 100, 50), (120, 255, 255))  # The HSV range for the color blue
-GREEN = ((40, 75, 150), (80, 255, 255))  # The HSV range for the color green
+GREEN = ((40, 150, 150), (80, 255, 255))  # The HSV range for the color green
 RED = ((0, 50, 50), (10, 255, 255))  # The HSV range for the color red
 
 WHITE = ((0, 60, 150), (179, 70, 255)) # The HSV range for the color white
 YELLOW = ((20, 0, 25), (40, 255, 255)) # The HSV range for the color yellow
 PURPLE = ((125, 50, 25), (165, 255, 255)) # The HSV range for the color purple
 BLACK = ((0, 50, 0), (179, 255, 56)) # The HSV range for the color black
-ORANGE = ((10, 50, 10), (20, 255, 255)) # The HSV range for the color orange
+ORANGE = ((10, 50, 50), (20, 255, 255)) # The HSV range for the color orange
 PINK = ((90, 150, 150), (179, 230, 255)) # The HSV range for the color pink
 
 # Color priority: Red >> Green >> Blue
@@ -89,7 +93,7 @@ class LineFollower(Controller):
       self.contour_center = None
       self.contour_area = 0
       COLOR_PRIORITY = (RED, GREEN, BLUE)
-      CROP_FLOOR = ((345, 0), (rc.camera.get_height()-20, rc.camera.get_width())) # crop floor for line detection
+      CROP_FLOOR = ((330, 0), (rc.camera.get_height()-20, rc.camera.get_width())) # crop floor for line detection
 
 
    def update_contour(self):
@@ -128,9 +132,16 @@ class LineFollower(Controller):
          cv.circle(line_img, (self.contour_center[1], self.contour_center[0]), 4, (255, 255, 0), -1)
          cv.drawContours(line_img, [c for c in contours if cv.contourArea(c) > MIN_CONTOUR_AREA]
                            , -1, (255, 255, 0), 2)
+      else: # if there are no contours, then set contour area = 0 and contour center = None
+      # Doing this prevents integrator windup since there is no extra error being calculated or summed up
+         self.contour_area = 0
+         self.contour_center = None
          
+      # cv.circle(image, (rc.camera.get_width()//2, rc.camera.get_height()//2), 6, (255, 0, 255), -1)
+
+      image[CROP_FLOOR[0][0]:CROP_FLOOR[1][0], 
+            CROP_FLOOR[0][1]:CROP_FLOOR[1][1]] = line_img
       
-      image[CROP_FLOOR[0][0]:CROP_FLOOR[1][0], CROP_FLOOR[0][1]:CROP_FLOOR[1][1]] = line_img
       rc.display.show_color_image(image)
 
    def update(self) -> tuple[float, float]: # returns the new speed and angle
@@ -143,22 +154,28 @@ class LineFollower(Controller):
       global COLOR_PRIORITY
       global Kp, Ki, Kd
       global prevError
-      global sum # for integral term
+      global error_sum # for integral term
+      global P, I, D
 
       self.update_contour()
 
-      if self.contour_center is not None:
+      # keeps error the same if there is no contour_center visible
+      # also prevents integrator windup
+      if self.contour_center is not None: 
          error = (rc.camera.get_width() // 2) - self.contour_center[1]
          P = -Kp*error
          D = 0
          if prevError is not None:
             D = -Kd * ((error - prevError)/rc.get_delta_time())
+         error_sum+=error*rc.get_delta_time()
+         I = error_sum*-Ki
          
-         angle = rc_utils.clamp(P + D, -1, 1)
+         prevError = error
+         angle = rc_utils.clamp(P + I + D, -1, 1)
 
     # TODO Part 4: Determine the speed that the RACECAR should drive at. This may be static or
     # variable depending on the programmer's intent.
-         speed = 1 - abs(rc_utils.remap_range(error, -320, 320, -0.3, 0.3))
+         speed = 1
       
       return (speed, angle)
    
@@ -167,3 +184,13 @@ class LineFollower(Controller):
    
    def get_area(self):
       return self.contour_area
+   
+   def get_P_error(self):
+      global P
+      return P
+   def get_I_error(self):
+      global I
+      return I
+   def get_D_error(self):
+      global D
+      return D
